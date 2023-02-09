dd3098f01fa84984fa5ee8620ffe5644290e7c7c
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2023-02-08 22:53:28 -0300

    add/ player dto

---
+++ src/jogadores/dto/criar-jogador.dto.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class CriarJogadorDTO {
+    readonly telefoneCelular: string;
+    readonly email: string;
+    readonly nome: string;
+}
--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDTO } from './dto/criar-jogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
 
     @Post()
-    async criarAtualizarJogador() {
-        return JSON.stringify({
-            "nome": "Vinicius"
-        })
+    async criarAtualizarJogador(@Body() criaJogadorDTO: CriarJogadorDTO) {
+        const { email } = criaJogadorDTO;
+        return JSON.stringify(`{
+            "nome": ${email}
+        }`)
     }
 
 }
