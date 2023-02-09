7c075f119fa1e437de27c2a9b9c95accb680b296
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2023-02-08 22:06:09 -0300

    init repo

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post } from '@nestjs/common';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+
+    @Post()
+    async criarAtualizarJogador() {
+        return JSON.stringify({
+            "nome": "Vinicius"
+        })
+    }
+
+}
